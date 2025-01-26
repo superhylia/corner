75ab0b0dfddf45d0395c540390a74a63a3ea7841
superhylia <43157926+superhylia@users.noreply.github.com>
2025-01-26 00:17:30 -0600

    Update plugins.js

--- src/_config/plugins.js
+++ src/_config/plugins.js
@@ -4,11 +4,11 @@ import rss from '@11ty/eleventy-plugin-rss';
 import syntaxHighlight from '@11ty/eleventy-plugin-syntaxhighlight';
 import webc from '@11ty/eleventy-plugin-webc';
 import {eleventyImageTransformPlugin} from '@11ty/eleventy-img';
+import readingtime from '@myxotod/eleventy-plugin-readingtime';
 
 // custom
 import {markdownLib} from './plugins/markdown.js';
 import {drafts} from './plugins/drafts.js';
-import {readingtime} from './plugins/readingtime.js';
 
 // Custom transforms
 import {htmlConfig} from './plugins/html-config.js';
@@ -23,9 +23,9 @@ export default {
   syntaxHighlight,
   webc,
   eleventyImageTransformPlugin,
+  readingtime,
   markdownLib,
   drafts,
-  readingtime,
   htmlConfig,
   cssConfig,
   jsConfig
